253af852a3b9b6af7638299e10b260909089fa3b
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-30 09:41:57 -0800

    roboshop shell script

---
+++ catalogue.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+ID=$(id -u) #checking root user are not 
+TIMESTAMP=$(date +%F)
+LOGFILE="/tmp/$0-$TIMESTAMP.log" #storing log file
+
+R="\e[31m" # colours assign
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+MONGDB_HOST=mongodb.dwas.shop
+
+echo "script started and executing at $TIMESTAMP" &>>$LOGFILE
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo  -e "$2..$R failed $N"
+        exit 1
+    else 
+        echo -e "$2...$G scuess $N"
+
+    fi
+}
+
+if [ $ID -ne 0 ]  # its checking same root user are not
+then 
+    echo -e "$R ERROR :: Please Run This Script with Root Acess $N"
+    exit 1 # you can give other than Zero
+else 
+    echo  "you are root user"
+
+fi
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disable the nodejs"
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "Enable the nodejs"
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing the nodejs applicatio"
+id roboshop
+if [$? -ne 0]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "Roboshop user already exists $Y SKYPPING$N"
+fi
+mkdir -p /app &>> $LOGFILE # Is there ownt create ,otherwise it create
+VALIDATE $? "Craeting a directory"
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "Download the application code"
+cd /app &>> $LOGFILE
+VALIDATE $? "app"
+unzip -o /tmp/catalogue.zip &>> $LOGFILE #-o for overide
+VALIDATE $? "Unzip the catalogue file"
+npm install &>> $LOGFILE
+VALIDATE $? "Installing Dependencies"
+cp /home/centos/roboshop-shell/catalogue.services /etc/systemd/system/catalogue.service &>> $LOGFILE #use absolute,because catalogue.service exists there
+VALIDATE $? "copying catalogue service file"
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "catalogue dameon reload"
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "Enable the catalogue service"
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "Start the catalogue service"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "copying mongodb repo"
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "Installing mongodb shell"
+mongo --host $MONGDB_HOST </app/schema/catalogue.js
+VALIDATE $? "Loading catalogue data into mongodb"
