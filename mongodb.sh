ccbf661c34f363184640120558e830a01c84dfde
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-30 02:30:30 -0800

    roboshop shell script

--- mongodb.sh
+++ mongodb.sh
@@ -10,15 +10,13 @@ N="\e[0m"
 
 echo "script started and executing at $TIMESTAMP" &>>$LOGFILE
 
-VALIDATE()
-{
-
+VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo  -e "ERROR :: $2..$R failed $N"
+        echo  -e "$2..$R failed $N"
         #exit 1
     else 
-        echo -e "$2...  $G scuess $N"
+        echo -e "$2...$G scuess $N"
 
     fi
 }
@@ -28,19 +26,30 @@ then
     echo -e "$R ERROR :: Please Run This Script with Root Acess $N"
     exit 1 # you can give other than Zero
 else 
-    echo  "you are root user "
+    echo  "you are root user"
 
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
 VALIDATE $? "copied Mongodb repo"
+
 dnf install mongodb-org -y &>> $LOGFILE
+
 VALIDATE $? "Installing Mongodb"
+
 systemctl enable mongod  &>> $LOGFILE
+
 VALIDATE $? "Enable a mongodb" 
+
 systemctl start mongod  &>> $LOGFILE
+
 VALIDATE $? "Start the mongodb"
+
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
 VALIDATE "Remote acces to Mongodb"
+
 systemctl restart mongod &>> $LOGFILE
+
 VALIDATE "Restart the Mongodb"
