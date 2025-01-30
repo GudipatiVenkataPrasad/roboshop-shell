c36c3e995af96e5da1c3797e739d8088cf5cc82a
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-30 02:34:34 -0800

    roboshop shell script

--- mongodb.sh
+++ mongodb.sh
@@ -11,7 +11,7 @@ N="\e[0m"
 echo "script started and executing at $TIMESTAMP" &>>$LOGFILE
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [$1 -ne 0]
     then 
         echo  -e "$2..$R failed $N"
         #exit 1
